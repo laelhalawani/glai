39e9cdf1646605e26f559c8d76d8353226f7d37a
Lael Halawani <lael.halawani.qadev@gmail.com>
2024-01-03 21:51:23 +0100

    Update glai version to 0.0.15

--- glai/__init__.py
+++ glai/__init__.py
@@ -1,12 +1,12 @@
 from .ai import AutoAI, EasyAI
 from .back_end import LlamaAI, AIMessages, ModelDB, ModelData
-__version__ = "0.0.14"
+__version__ = "0.0.15"
 
 __all__ = ['AutoAI', 'EasyAI', 'LlamaAI', 'ModelDB', 'AIMessages', 'ModelData']
-print(f"""
-glai
-GGUF LLAMA AI - Package for simplified text generation with Llama models quantized to GGUF format is loaded.
-Provides high level APIs for loading models and generating text completions.
-For more information please check README.md file or visit https://github.com/laelhalawani/glai 
-Detailed API documentation can be found here: https://laelhalawani.github.io/glai/
-""")
+# print(f"""
+# glai
+# GGUF LLAMA AI - Package for simplified text generation with Llama models quantized to GGUF format is loaded.
+# Provides high level APIs for loading models and generating text completions.
+# For more information please check README.md file or visit https://github.com/laelhalawani/glai 
+# Detailed API documentation can be found here: https://laelhalawani.github.io/glai/
+# """)
