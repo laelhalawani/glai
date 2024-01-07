4ff19067c0ef9485f9e30f643d5cf3c756769f43
Lael Halawani <lael.halawani.qadev@gmail.com>
2024-01-07 22:27:45 +0100

    Update version number in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("./README.md", "r") as fh:
     long_description = fh.read()
 setup(
     name="glai",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         'requests>=2.31.0',
