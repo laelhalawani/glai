2b04ef8ad73e5d6bd08fb140fcc6daed071f6094
Lael Halawani <lael.halawani.qadev@gmail.com>
2024-01-13 19:50:02 +0100

    Update dependencies versions in requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("./README.md", "r") as fh:
     long_description = fh.read()
 setup(
     name="glai",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=[
         'requests>=2.31.0',
         'beautifulsoup4>=4.9.3',
-        'gguf_llama>=0.0.16',
+        'gguf_llama>=0.0.17',
         'gguf_modeldb>=0.0.2'
     ],
     include_package_data=True,
